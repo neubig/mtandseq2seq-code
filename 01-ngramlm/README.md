688d32103f2abf11369ea292944fbf8cd3b9a0f1
Graham Neubig <neubig@gmail.com>
2019-08-29 07:00:01 -0400

    Updates to n-gram LM

--- 01-ngramlm/README.md
+++ 01-ngramlm/README.md
@@ -2,6 +2,9 @@
 by Graham Neubig
 
 This is an example of an interpolated bi-gram language model.
+
+## Basic Usage
+
 Usage:
 
     python ngram-lm.py train_data.txt test_data.txt
@@ -9,3 +12,17 @@ Usage:
 For example, on the example data in the top directory:
 
     python ngram-lm.py ../en-de/train.en-de.low.en ../en-de/valid.en-de.low.en
+
+You can also set some hyper-parameters
+
+    python ngram-lm.py --uni_prob 0.3 --unk_prob 0.005 ../en-de/train.en-de.low.en ../en-de/valid.en-de.low.en
+
+## Advanced Examples
+
+You can perform grid search to find the best interpolation coefficients, an example is shown in `grid_search.sh`
+
+    bash grid_search.sh
+
+You can also print out the probabilities of each word:
+
+    python ngram-lm.py --print_probs ../en-de/train.en-de.low.en ../en-de/valid.en-de.low.en > probs.txt
--- 01-ngramlm/ngram-lm.py
+++ 01-ngramlm/ngram-lm.py
@@ -3,17 +3,35 @@
 
 import sys
 import math
+import argparse
 from collections import defaultdict
 
-N = 2
-VOCAB_SIZE = 10000000
-UNK_ALPHA = 0.01
-UNI_ALPHA = 0.25
-BI_ALPHA = 1.0 - UNK_ALPHA - UNI_ALPHA
+p = argparse.ArgumentParser(description="""Code to train an extremely simple linearlly interpolated bigram language model""")
+p.add_argument("train_file", help="The file to train on")
+p.add_argument("test_file", help="The file to test on")
+p.add_argument("--print_probs", action="store_true", help="Whether to print probabilities for each word")
+p.add_argument("--skip_unk", action="store_true", help="Skip unknown words in calculating probabilities")
+p.add_argument("--vocab_size", type=int, help="Total size of the vocabulary", default=1e7)
+p.add_argument("--unk_alpha", type=float, help="The amount of probability to assign to unknown words", default=0.01)
+p.add_argument("--uni_alpha", type=float, help="The amount of probability to assign to unigrams", default=0.20)
+args = p.parse_args()
 
+N = 2
+VOCAB_SIZE = args.vocab_size
+UNK_ALPHA = args.unk_alpha
+UNI_ALPHA = args.uni_alpha
+
+# If we're skipping unknown words, set the interpolation probability of unknowns to zero
+if args.skip_unk:
+  BI_ALPHA = 1.0 - UNI_ALPHA
+  UNK_ALPHA = 0.0
+else:
+  BI_ALPHA = 1.0 - UNK_ALPHA - UNI_ALPHA
+
+# Read in the training data
 train_counts = defaultdict(lambda: 0)
 train_ctxts = defaultdict(lambda: 0)
-with open(sys.argv[1], "r") as f:
+with open(args.train_file, "r") as f:
   for line in f:
     sent = line.strip().split(" ") + ["<s>"]
     ngram = ["<s>"] * N
@@ -24,22 +42,35 @@ with open(sys.argv[1], "r") as f:
         train_ctxts[tuple(ctxt[i:])] += 1
         train_counts[tuple(ngram[i:])] += 1
 
+# Calculate on test
 alpha = [UNK_ALPHA, UNI_ALPHA, BI_ALPHA]
 lls = 0
 words = 0
-with open(sys.argv[2], "r") as f:
+with open(args.test_file, "r") as f:
   for line in f:
     sent = line.strip().split(" ") + ["<s>"]
     ngram = ["<s>"] * N
     for word in sent:
       ctxt = ngram[1:]
       ngram = ctxt + [word]
-      total = alpha[0] / VOCAB_SIZE
-      for i in range(N):
+      all_probs = [1.0 / VOCAB_SIZE]
+      for i in range(N)[::-1]:
         if tuple(ngram[i:]) in train_counts:
-          total += alpha[N-i] * train_counts[tuple(ngram[i:])] / train_ctxts[tuple(ctxt[i:])]
-      lls += math.log(total)
-    words += len(sent)-1  
+          all_probs.append(train_counts[tuple(ngram[i:])] / train_ctxts[tuple(ctxt[i:])])
+        else:
+          all_probs.append(0.0)
+      total = 0.0
+      for prob, alph in zip(all_probs, alpha):
+        total += prob * alph
+      if args.print_probs:
+        print(' '.join([str(x) for x in [word]+all_probs+[total]]))
+      if not (args.skip_unk and total == 0.0):
+        lls += math.log(total)
+    if args.print_probs:
+      print()
+    words += len(sent)-1
+
+# Print out the results
 my_score = math.exp(-lls/words)
-print ("perplexity at alpha=%r: %f" % (alpha, my_score))
+print ("perplexity at alpha=%r: %f" % (alpha, my_score), file=sys.stderr)
 
